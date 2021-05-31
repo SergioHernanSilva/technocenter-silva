86775eb9a9ca062f5e5ca4d53ee4a8fbdcf0e143
neneeok <sergiohernan.silva@gmail.com>
2021-05-31 19:38:38 -0300

    Se generó NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import Reacts from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <>
+            <h1>Techno Center</h1>
+            <p><a href='#'>PRODUCTOS</a></p>
+            <p><a href='#'>MI CUENTA</a></p>
+            
+        </>
+    )
+};
+
+export default NavBar;
